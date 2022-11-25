c50773f0ecbdc5b0aaa632aca590781bf8fe2a30
Jessica Espejo <jenespejoto@unal.edu.co>
2022-11-25 11:08:55 -0500

    Inclusión de opciones aleatorias para el computador y estandarización de seleccion de usuario

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-user_option = input("Por favor ingrese: piedra, papel o tijera ->")
-computer_option = "piedra"
+from random import choice
 
-if user_option == computer_option:
+#opcion seleccionada por el Usuario
+user_option = input("Realice su elección: Piedra, papel o tijera ->")
+#print(type(user_option))
+user_option_upper = user_option.upper()
+
+#opcion seleccionada por el compurador 
+options = ["PIEDRA","PAPEL", "TIJERA"]
+computer_option = choice(options)
+print("El computador eligió ", computer_option)
+
+if user_option_upper == computer_option:
     print("Empate!")
-elif user_option == "piedra":
-    if computer_option == "tijera":
+elif user_option_upper == "PIEDRA":
+    if computer_option == "TIJERA":
         print("Piedra gana a tijera")
         print("Usuario ganó!")
     else:
         print("Papel gana a piedra")
         print("Computador ganó!")
-elif user_option == "papel":
-    if computer_option == "tijera":
+elif user_option_upper == "PAPEL":
+    if computer_option == "TIJERA":
         print("Tijera gana a papel")
         print("Computador ganó!")
     else:
         print("Papel gana a piedra")
         print("Usuario ganó!")
-elif user_option == "tijera":
-    if computer_option == "papel":
+elif user_option_upper == "TIJERA":
+    if computer_option == "PAPEL":
         print("Tijera gana a papel")
         print("Usuario ganó!")
     else:
